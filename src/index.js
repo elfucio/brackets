532f90d534284bf9cc9daee20150b089b62bee35
Elya <elfucio@gmail.com>
2020-03-17 21:47:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = bracketsConfig.map(item => item.join(''));
+  for (let i = 0; i < bracketsConfig.length;) {
+    if (str.includes(arr[i])) {
+      str = str.replace(arr[i], '');
+      i = 0;
+    } else i++;
+  }
+  if (str.length == 0) {
+    return true;
+  } else return false;
+};
